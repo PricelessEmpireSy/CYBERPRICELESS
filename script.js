6f3df9aa4022883d82833b107897a0f15327b113
PricelessEmpireSy <misspriceless45@gmail.com>
2025-01-30 16:37:27 +0100

    Added new features

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ document.addEventListener('DOMContentLoaded', function() {
         return false; // prevent actual form submission
     };
 });
+
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    if (n >= slides.length) { slideIndex = 0; }
+    if (n < 0) { slideIndex = slides.length - 1; }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
